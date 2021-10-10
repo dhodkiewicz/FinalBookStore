0c792019fc9f0ff84f9d05f7f9ee7a597567bf9c
oracleofoz92 <45974974+dhodkiewicz@users.noreply.github.com>
2021-10-10 09:22:57 -0500

    Add project files.

---
+++ FinalBookStore/Scripts/Site.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(".btnHide").click(function () {
+        $(".gradient-text").hide(1000);
+    });
+    $(".btnShow").click(function () {
+        $(".gradient-text").show(1000);
+    });
+});
